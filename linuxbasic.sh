259677cee04206553e15f3982f45d6f8991e599f
scwalle <87709706+scwalle@users.noreply.github.com>
2023-11-02 14:52:54 -0500

    Update linuxbasic.sh

--- linuxbasic.sh
+++ linuxbasic.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# update everything
+echo update everything
 sudo apt-get upgrade && sudo apt-get update && sudo apt-get dist-upgrade
 
-# remove hacking programs
+echo enable firewall
+apt-get install ufw && ufw enable
+
+echo remove hacking programs
 # add more probably
 pkgstoremove=( hashcat apache2 john )
 
@@ -11,3 +14,17 @@ for package in $pkgstoremove; do
 sudo apt-get purge package
 done
 
+
+echo remove unneccesary media files
+# add more probably
+extstoremove=( mov mp4 mp3 jpg jpeg )
+
+for ext in $extstoremove; do
+find / -name "*.$ext" -type f -delete
+done
+
+
+apt-cache rdepends <package>
+
+#delete files with nonexistant owners
+find / -nogroup -nouser -delete
